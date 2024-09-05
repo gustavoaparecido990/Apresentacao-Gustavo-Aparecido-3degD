dab0d41ea82363f780784880ad1eee8d9d4a5ffb
gustavoaparecido990 <164099470+gustavoaparecido990@users.noreply.github.com>
2024-09-05 02:26:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const primeiroNome = "Gustavo";
+const sobrenome = "Aparecido"
+const idade = 17;
+const altura = 1.73;
+const cidadeNatal = "Curitiba";
+const massa = 62
+
+console.log("O meu primeiro nome é:")
+console.log(primeiroNome);
+console.log("O meu sobrenome é:")
+console.log(sobrenome);
+console.log("A minha idade é:")
+console.log(idade);
+console.log("A minha altura é:")
+console.log(altura);
+console.log("A minha cidade natal é:")
+console.log(cidadeNatal);
+console.log("Minha massa é:")
+console.log(massa);
